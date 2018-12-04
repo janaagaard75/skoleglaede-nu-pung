b850365e6da24cb7799f445b8dd806d81efc114b
Jan Aagaard <jan@aagaard.net>
2018-12-04 21:54:01 +0100

    Remove empty line

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,5 @@ import { ScannerScreen } from './ScannerScreen'
 export default createStackNavigator({
   // tslint:disable:object-literal-sort-keys
   Main: { screen: MainScreen },
-
   BarCodeScanner: { screen: ScannerScreen }
 })
