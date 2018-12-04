7e2d346bbdcaa36359c11d4673dd8eb5f7241743
Jan Aagaard <jan@aagaard.net>
2018-12-04 21:44:09 +0100

    Add total amount

--- src/MainScreen.tsx
+++ src/MainScreen.tsx
@@ -3,8 +3,21 @@ import { Button } from 'react-native'
 import { Component } from 'react'
 import { NavigationScreenProps } from 'react-navigation'
 import { ScrollView } from 'react-native'
+import { Text } from 'react-native'
 
-export class MainScreen extends Component<NavigationScreenProps> {
+interface State {
+  totalAmount: number
+}
+
+export class MainScreen extends Component<NavigationScreenProps, State> {
+  constructor(props: NavigationScreenProps, context?: any) {
+    super(props, context)
+
+    this.state = {
+      totalAmount: 0
+    }
+  }
+
   public static navigationOptions = {
     title: 'Skoleglæde.nu pung'
   }
@@ -17,11 +30,25 @@ export class MainScreen extends Component<NavigationScreenProps> {
           flex: 1
         }}
       >
+        <Text
+          style={{
+            fontSize: 30,
+            marginBottom: 20,
+            marginTop: 30,
+            textAlign: 'center'
+          }}
+        >
+          {MainScreen.formatAsCurrency(this.state.totalAmount)}
+        </Text>
         <Button
           onPress={() => this.props.navigation.navigate('BarCodeScanner')}
           title={'Scan QR kode'}
         />
       </ScrollView>
     )
+  }
+
+  private static formatAsCurrency(input: number): string {
+    return Intl.NumberFormat('da-DK', { style: 'currency', currency: 'DKK' }).format(input)
   }
 }
