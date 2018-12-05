eab659db0f94e219834fa51eb0be9c101afeaff3
Jan Aagaard <jan@aagaard.net>
2018-12-05 07:50:54 +0100

    Use toLocalString

--- src/Formatter.ts
+++ src/Formatter.ts
@@ -1,5 +1,5 @@
 export class Formatter {
   public static formatAsCurrency(input: number): string {
-    return Intl.NumberFormat('da-DK', { style: 'currency', currency: 'DKK' }).format(input)
+    return input.toLocaleString('da-DK', { style: 'currency', currency: 'DKK' })
   }
 }
