b396420db68b4f1713835de579637266d26c9b63
Jan Aagaard <jan@aagaard.net>
2019-07-14 16:02:01 +0200

    Use '* as React' syntax

--- src/MainScreen.tsx
+++ src/MainScreen.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import * as React from "react";
 import { Component } from "react";
 import { Dimensions } from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
--- src/ScannerScreen.tsx
+++ src/ScannerScreen.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import * as React from "react";
 import { BarCodeScanner } from "expo";
 import { Component } from "react";
 import { Dimensions } from "react-native";
